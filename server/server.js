2594721cd5ab7ca84f910916878d5f6b986e8548
Cristhian Avila <cyavilap@hotmail.com>
2020-10-15 15:08:39 -0500

    server port env updated

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser')
 var app = express()
 const userInfo = require('./modules/userInfo.js')
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
