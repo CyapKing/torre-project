d43227586e03bff971fba85a4cb5dc727f74b5d9
Cristhian Avila <cyavilap@hotmail.com>
2020-10-14 20:43:19 -0500

    added styles and card component

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,14 @@ const routes = [
     component: Home
   },
   {
+    path: '/user-profile',
+    name: 'UserProfile',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "user-profile" */ '../views/UserProfile.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
+  <vs-card id="user-card" type="3">
+    <template #title>
+      <h3>Usuarios</h3>
+    </template>
+    <template #img>
+      <div style="background-color: #00aae4; border-radius: 15px">
+        <img :src="UserImage" alt="" />
+      </div>
+    </template>
+    <template #text>
+      <div class="description-container" />
+      <p>Busca a un usuario por su nombre</p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class="bx bx-heart"></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class="bx bx-chat"></i>
+        <span class="span"> 54 </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+  <!-- <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div> -->
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import UserImage from '@/assets/user.png'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data () {
+    return {
+      UserImage
+    }
   }
 }
 </script>
+<style lang="scss">
+#user-card {
+  .description-container {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
